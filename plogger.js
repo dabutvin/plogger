16f8948c1e77816eeafd66b47830b19c68920995
dabutvin <dabutvin@microsoft.com>
2013-11-17 12:15:42 -0800

    timer

--- plogger.js
+++ plogger.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+    startTimer();
 });
+
+function startTimer(){
+    $("#timer").animate({
+        "height": "0%"
+    }, 7000, function(){
+        alert("Time!");
+    });
+}
