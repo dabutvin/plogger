6918e9d7efbc4c8de0cac575e0aecd30582ca429
dabutvin <dabutvin@microsoft.com>
2013-11-18 09:49:04 -0800

    local jQuery. Initial blocks. toggle timer

--- plogger.js
+++ plogger.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
 
-    startTimer();
+    $("#timer-toggle").click(function(){
+        startTimer();
+    });
 });
 
+
 function startTimer(){
-    $("#timer").animate({
+    var timer = $("#timer");
+
+    timer.animate({
         "height": "0%"
     }, 7000, function(){
         alert("Time!");
+    });
+
+    $("#timer-toggle").unbind("click")
+    .text("Stop Timer").click(function(){
+        stopTimer();
+    });
+}
+
+function stopTimer(){
+    var timer = $("#timer");
+
+    timer.stop();
+
+    $("#timer-toggle").unbind("click")
+    .text("Start Timer").click(function(){
+        startTimer();
     });
 }
