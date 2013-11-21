30e212ec4762356dbcd897cab1105a1cf7913019
dabutvin <dabutvin@microsoft.com>
2013-11-21 09:31:33 -0800

    start building board

--- plogger.js
+++ plogger.js
@@ -1,41 +1,85 @@
-$(document).ready(function(){
-
-    $("#timer-toggle").click(function(){
-        startTimer();
-    });
-
-    $(".block").click(function(){
-        blockClick($(this));
-    })
-});
+    Plogger = function($){
 
 
-function startTimer(){
-    var timer = $("#timer");
+        var bindEvents = function(){
+            $("#timer-toggle").click(function(){
+                startTimer();
+            });
 
-    timer.animate({
-        "height": "0%"
-    }, 7000, function(){
-        alert("Time!");
-    });
+            $(".block").click(function(){
+                blockClick($(this));
+            })
+        };
 
-    $("#timer-toggle").unbind("click")
-    .text("Stop Timer").click(function(){
-        stopTimer();
-    });
-}
+        var buildBoard = function(){
+            var block = $("<li class='block' />"),
+                blocks = $("#blocks"),
+                numBlocks = calculateBoardSize(),
+                i;
 
-function stopTimer(){
-    var timer = $("#timer");
+            for(i=1; i<numBlocks; i++){
+                blocks.prepend(block.attr("data-spot",i).clone());
+            }
+        };
 
-    timer.stop();
+        var calculateBoardSize = function(){
+            return 77;
+        };
 
-    $("#timer-toggle").unbind("click")
-    .text("Start Timer").click(function(){
-        startTimer();
-    });
-}
+        var startTimer = function(){
+            var timer = $("#timer");
 
-function blockClick(jq){
-    $("#man").appendTo(jq);
-}
+            timer.animate({
+                "height": "0%"
+            }, 7000, function(){
+                alert("Time!");
+            });
+
+            $("#timer-toggle").unbind("click")
+            .text("Stop Timer").click(function(){
+                stopTimer();
+            });
+        };
+
+        var stopTimer = function(){
+            var timer = $("#timer");
+
+            timer.stop();
+
+            $("#timer-toggle").unbind("click")
+            .text("Start Timer").click(function(){
+                startTimer();
+            });
+        };
+
+        var blockClick = function(jq){
+            var clickedSpot = jq.attr("data-spot");
+            if (settings.manSpot < clickedSpot) {
+                settings.manSpot++;
+            } else if (settings.manSpot > clickedSpot) {
+                settings.manSpot--;
+            }
+            moveMan();
+        };
+
+        var moveMan = function(){
+            var man = $("#man"),
+                block = $(".block[data-spot='"+settings.manSpot+"']");
+
+            block.append(man);
+        };
+
+        var settings = {
+            freeze : false,
+            manSpot : 0,
+            windowWidth : $("window").width(),
+            blocksWidthPercent : 0.89,
+            blockWidthPercent : 0.09
+        };
+
+        return{
+            BindEvents: bindEvents,
+            BuildBoard: buildBoard
+        };
+
+}(jQuery);
