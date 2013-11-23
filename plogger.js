91ef924ae4e04d17e4d493da5861d26fe3a95d26
dabutvin <dabutvin@microsoft.com>
2013-11-22 17:54:57 -0800

    Better movement

--- plogger.js
+++ plogger.js
@@ -3,6 +3,10 @@
         var init = function(){
             buildBoard();
             bindEvents();
+            $(".block[data-spot='5']").addClass("block-built");
+            // $("#man").draggable({
+            //     axis: "x"
+            // });
         }
 
         var bindEvents = function(){
@@ -52,13 +56,35 @@
         };
 
         var blockClick = function(jq){
-            var clickedSpot = jq.attr("data-spot");
-            if (settings.manSpot < clickedSpot) {
-                settings.manSpot++;
-            } else if (settings.manSpot > clickedSpot) {
-                settings.manSpot--;
-            }
+            var clickedSpot = jq.attr("data-spot"),
+                clickedCol = getColumn(clickedSpot);
+
+            setManSpot(clickedCol);
             moveMan();
+        };
+
+        var setManSpot = function(clickedCol){
+            var currentColumn = getColumn(settings.manSpot),
+                nextColumn = currentColumn;
+
+            if (currentColumn < clickedCol) {
+                nextColumn = currentColumn + 1;
+            } else if(currentColumn > clickedCol){
+                nextColumn = currentColumn - 1;
+            } else{
+
+            }
+
+            settings.manSpot = firstAvailableBlock(nextColumn);
+        };
+
+        var firstAvailableBlock = function(column){
+            var block = $(".block[data-spot='"+ column +"']");
+            if (block.hasClass("block-built")) {
+                return firstAvailableBlock(column + settings.spotsPerRow);
+            } else{
+                return column;
+            }
         };
 
         var moveMan = function(){
@@ -68,10 +94,15 @@
             block.append(man);
         };
 
+        var getColumn = function(block){
+            return block % settings.spotsPerRow;
+        }
+
         var settings = {
             freeze : false,
             manSpot : 0,
             numSpots : 99,
+            spotsPerRow : 11
         };
 
         return{
